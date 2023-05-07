f3fbb36156ecd7dfbbe0b7a76b4ecddf6de05ccb
Chenghsuan Ho <chengxuanjhe@gmail.com>
2023-05-07 17:22:47 +0800

    Add pnpm path configuration

--- common/.config/fish/config.fish
+++ common/.config/fish/config.fish
@@ -3,3 +3,8 @@ fish_vi_key_bindings
 
 # starship [https://github.com/starship/starship]
 starship init fish | source
+
+# pnpm
+set -gx PNPM_HOME "/home/cheng/.local/share/pnpm"
+set -gx PATH "$PNPM_HOME" $PATH
+# pnpm end
