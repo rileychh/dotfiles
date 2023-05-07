9f36ad4b7f418d14802ac913d51ff38bca798932
Chenghsuan Ho <chengxuanjhe@gmail.com>
2023-05-07 17:58:18 +0800

    Remove extended view from ll and la

--- common/.config/fish/functions/ll.fish
+++ common/.config/fish/functions/ll.fish
@@ -1,3 +1,3 @@
 function ll --description 'List contents of directory using long format'
-    exa -lbghFm@ --icons --color-scale --group-directories-first --git --time-style=iso $argv
+    exa -lbghFm --icons --color-scale --group-directories-first --git --time-style=iso $argv
 end
--- common/.config/fish/functions/ls.fish
+++ common/.config/fish/functions/ls.fish
@@ -1,3 +1,3 @@
 function ls --description 'List contents of directory'
-    exa -bghFm@ --icons --color-scale --group-directories-first --git --time-style=iso $argv
+    exa -bghFm --icons --color-scale --group-directories-first --git --time-style=iso $argv
 end
