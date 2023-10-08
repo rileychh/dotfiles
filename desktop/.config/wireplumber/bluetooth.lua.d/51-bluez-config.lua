23fe0f860102bdfb2d5059d3f924c04d6c051573
Chenghsuan Ho <chengxuanjhe@gmail.com>
2023-10-08 23:29:34 +0800

    Add Bluetooth headphone config

---
+++ desktop/.config/wireplumber/bluetooth.lua.d/51-bluez-config.lua
@@ -0,0 +1,5 @@
+bluez_monitor.properties = {
+	["bluez5.enable-sbc"] = false, -- Use SBC-XQ or AAC
+	["bluez5.headset-roles"] = "[ ]", -- Don't auto switch to headset mode
+	["bluez5.hfphsp-backend"] = "none", -- Disable HFP/HSP
+}
