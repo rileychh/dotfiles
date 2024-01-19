f4321800ccacaea956ca650bb64bdcc8666a5e8c
Chenghsuan Ho <chengxuanjhe@gmail.com>
2024-01-19 13:05:27 +0800

    Remove cp and mv functions

--- common/.config/fish/functions/cp.fish
+++
@@ -1,3 +0,0 @@
-function cp
-    rsync --archive -hh --partial --info=stats1,progress2 --modify-window=1 $argv
-end
--- common/.config/fish/functions/mv.fish
+++
@@ -1,3 +0,0 @@
-function mv
-    rsync --archive -hh --partial --info=stats1,progress2 --modify-window=1 --remove-source-files $argv
-end
