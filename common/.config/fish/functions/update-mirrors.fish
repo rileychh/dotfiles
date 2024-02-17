a021619522b19816a33e0b96ef3ce89013715a2e
Chenghsuan Ho <chengxuanjhe@gmail.com>
2024-02-17 17:24:47 +0800

    Handle errors in update-mirrors

--- common/.config/fish/functions/update-mirrors.fish
+++ common/.config/fish/functions/update-mirrors.fish
@@ -1,8 +1,17 @@
 function update-mirrors
-	set mirrorlist /etc/pacman.d/mirrorlist
-	set temp (mktemp)
-	sudo true # Ask for password first
-	rate-mirrors arch | tee $temp
-	and sudo mv $mirrorlist $mirrorlist-old
-	and sudo mv $temp $mirrorlist
+    sudo true # Ask for password first
+
+    set temp (sudo mktemp)
+    rate-mirrors arch | sudo tee $temp
+
+    if test $pipestatus[1] -ne 0
+        echo (set_color red)"Failed to update mirrorlist"(set_color normal)
+        sudo rm $temp
+        return 1
+    end
+
+    set mirrorlist /etc/pacman.d/mirrorlist
+    sudo mv $mirrorlist $mirrorlist-old
+    sudo mv $temp $mirrorlist
+    sudo chmod 644 $mirrorlist
 end
