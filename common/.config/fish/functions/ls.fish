7aaf98b1c30d269ca64e2c3d760d223dac568806
Chenghsuan Ho <chengxuanjhe@gmail.com>
2024-02-17 17:23:43 +0800

    Fix the "-F" flag taking an argument in eza

--- common/.config/fish/functions/ls.fish
+++ common/.config/fish/functions/ls.fish
@@ -1,7 +1,7 @@
 function ls --description 'List contents of directory'
     if command --query eza
-        eza -bghFm --icons --color-scale=all --group-directories-first --git --time-style=iso $argv
+        eza -bghmF --icons --color-scale=all --group-directories-first --git --time-style=iso $argv
     else
-        exa -bghFm --icons --color-scale --group-directories-first --git --time-style=iso $argv
+        exa -bghmF --icons --color-scale --group-directories-first --git --time-style=iso $argv
     end
 end
