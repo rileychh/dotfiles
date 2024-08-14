22ff68dd16dbcc63a85f4e8251ace8f1eccf56e6
Riley Ho <riley@rileychh.dev>
2024-08-14 20:11:11 +0800

    Fix up-merge-sync function

--- common/.config/fish/functions/up-merge-sync.fish
+++ common/.config/fish/functions/up-merge-sync.fish
@@ -2,9 +2,12 @@ function up-merge-sync
     set pr $argv[1]
 
     gh pr update-branch $pr --rebase
-    and gh pr merge $pr -md
+    and gh pr merge $pr --merge
+    and git stash
     and git switch main
     and gt sync
+    and gt submit --stack --update-only
     and git switch -
+    and git stash pop
 end
 
