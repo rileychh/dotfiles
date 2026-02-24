48bd29ad74a314e22fbeeca5be988c1626a3da75
Riley Ho <riley@rileychh.dev>
2026-02-24 23:09:44 +0800

    Add npm bin directory to PATH

--- home/dot_config/fish/conf.d/paths.fish
+++ home/dot_config/fish/conf.d/paths.fish
@@ -1,5 +1,6 @@
 fish_add_path $HOME/.local/bin
 fish_add_path $XDG_DATA_HOME/cargo/bin
 fish_add_path $XDG_DATA_HOME/go/bin
+fish_add_path $XDG_DATA_HOME/npm/bin
 fish_add_path $PNPM_HOME
 set PATH node_modules/.bin $PATH
