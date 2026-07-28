c72ff7c4af769ca3b498df23062f47c26779fbac
Riley Ho <riley@rileychh.dev>
2026-07-28 17:56:23 +0800

    Fix Finicky browser names and add alternative URL rewrites

--- home/dot_config/finicky/finicky.ts
+++ home/dot_config/finicky/finicky.ts
@@ -8,24 +8,54 @@ export default {
   },
   handlers: [
     {
-      match: "instagram.com/*",
-      browser: "Instagram",
-    },
-    {
       match: "www.facebook.com/*",
       browser: "Facebook",
     },
     {
       match: ["maps.app.goo.gl/*", "www.google.com/maps/*"],
-      browser: "Google Maps",
+      browser: "Google 地圖",
     },
     {
-      match: "threads.com/*",
-      browser: "Threads",
+      match: "instagram.com/*",
+      browser: "Instagram",
     },
     {
       match: "chat.coscup.org/*",
       browser: "Mattermost",
     },
+    {
+      match: "*.threads.com/*",
+      browser: "Threads",
+    },
+    {
+      match: [
+        "x.com/*",
+        "twitter.com/*"
+      ],
+      browser: "Twitter",
+    },
   ],
+  rewrite: [
+    {
+      match: "facebed.rileychh.dev/*",
+      url: url => {
+        url.host = "www.facebook.com"
+        return url
+      }
+    },
+    {
+      match: "fixthreads.seria.moe/*",
+      url: url => {
+        url.host = "www.threads.com"
+        return url
+      }
+    },
+    {
+      match: "fxtwitter.com/*",
+      url: url => {
+        url.host = "x.com"
+        return url
+      }
+    }
+  ]
 } satisfies FinickyConfig;
